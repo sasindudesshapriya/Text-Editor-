4e035fd186842c33f331737816cefd635816c62a
sasindudesshapriya <152970418+sasindudesshapriya@users.noreply.github.com>
2023-12-05 17:24:55 +0530

    add Feature-undo

--- App.js
+++ App.js
@@ -27,3 +27,24 @@ $('.left').on('click',function(){
   $('.justify').on('click', function() {
     document.getElementById('editor').style.textAlign = 'justify '
   })
+  var contentHistory = []
+ var currentStep = 0
+
+ function undo() {
+    if (currentStep > 0) {
+        currentStep--
+        if (contentHistory[currentStep] != undefined) {
+            document.getElementById('editor').innerHTML = contentHistory[currentStep]
+        }
+    }
+ }
+ document.querySelectorAll ('#editor').forEach(function (element) {
+    element.addEventListener('input', function() {
+        currentStep++
+        if (currentStep < contentHistory.length){
+            contentHistory = contentHistory.slice(0, currentStep)
+        }
+
+        contentHistory.push(document.getElementById('editor').innerHTML)
+    })
+ })
